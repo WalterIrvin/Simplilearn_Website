b90246d4916bf5aba8da910846e41d5cbb247319
WalterIrvin <walterdirvin@gmail.com>
2020-10-30 10:51:58 -0400

    basic website

---
+++ js/postBlog.js
@@ -0,0 +1,22 @@
+function signup(){
+    window.location = "form-validation.html"
+}
+
+function openPost(num){
+    if (num == 0){
+        // pie
+        window.location = "pie.html";
+    }
+    if (num == 1){
+        // chicken
+        window.location = "chicken.html";
+    }
+    if (num == 2){
+        // pork
+        window.location = "pork.html";
+    }
+    if (num == 3){
+        // fry
+        window.location = "fry.html";
+    }
+}
